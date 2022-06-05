348d1867f6f4ed254b605b0c25d077fdd9e22192
rudraksh <rudrakshtandon@gmail.com>
2022-06-05 17:24:39 +0530

    initial commit

--- app/src/main/java/com/ieeeias/vit_finders/MainActivity.java
+++ app/src/main/java/com/ieeeias/vit_finders/MainActivity.java
@@ -1,6 +1,16 @@
 package com.ieeeias.vit_finders;
 
-public class MainActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
 
 }
