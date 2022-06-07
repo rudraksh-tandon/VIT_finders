698366113419c5b7e45ce6c2950778f4712e2ee5
rudraksh <rudrakshtandon@gmail.com>
2022-06-07 23:04:25 +0530

    splash screen added

---
+++ app/src/main/java/com/ieeeias/vit_finders/SplashScreen.java
@@ -0,0 +1,61 @@
+package com.ieeeias.vit_finders;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.splash_screen_activity);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
+
+//public class SplashScreen extends AppCompatActivity {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.splash_screen_activity);
+//        try {
+//            VideoView videoHolder = new VideoView(this);
+//            setContentView(videoHolder);
+//            Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.app_intro_2);
+//            videoHolder.setVideoURI(video);
+////            videoHolder.start();
+//
+//            videoHolder.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//                public void onCompletion(MediaPlayer mp) {
+//                    jump();
+//                }
+//            });
+//            videoHolder.start();
+//        } catch (Exception ex) {
+//            jump();
+//        }
+//    }
+//
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        jump();
+//        return true;
+//    }
+//
+//    private void jump() {
+//        if (isFinishing())
+//            return;
+//        startActivity(new Intent(this, MainActivity.class));
+//        finish();
+//    }
+//}
